53181ddc8d9f84b404f20e1bafbb7b61133f96cd
noeldiazven <celso.diaz.ventura@gmail.com>
2026-09-18 15:29:35 -0500

    fix: adaptar proyecto a Prisma 7

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,10 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
-<<<<<<< HEAD
-import { PrismaModule } from './vehiculos/prisma/prisma.module.js';
-import { VehiculosModule } from './vehiculos/vehiculos.module.js';
-=======
 import { PrismaModule } from './prisma/prisma.module.js';
 import { RepuestosModule } from './repuestos/repuestos.module.js';
 import { DetalleRepuestosModule } from './detalle-repuestos/detalle-repuestos.module.js';
->>>>>>> bbf4a5e1267a76680c56519039ded4c1baad5936
+import { VehiculosModule } from './vehiculos/vehiculos.module.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -21,12 +17,9 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       serviceId: 'AutoFix-NestJS',
     }),
     PrismaModule,
-<<<<<<< HEAD
     VehiculosModule,
-=======
     RepuestosModule,
     DetalleRepuestosModule,
->>>>>>> bbf4a5e1267a76680c56519039ded4c1baad5936
   ],
   controllers: [AppController],
   providers: [AppService],
