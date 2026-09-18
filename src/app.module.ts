92800048ec344aaef99bad7219ff627026b7d9d0
Felipe Arroyo <felipevieira.arroyo@gmail.com>
2026-09-18 17:31:47 -0300

    fix: resolver conflito do Prisma 7 e módulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,11 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
-<<<<<<< HEAD
-import { PrismaModule } from './vehiculos/prisma/prisma.module.js';
-import { VehiculosModule } from './vehiculos/vehiculos.module.js';
-=======
+
 import { PrismaModule } from './prisma/prisma.module.js';
+import { VehiculosModule } from './vehiculos/vehiculos.module.js';
 import { RepuestosModule } from './repuestos/repuestos.module.js';
 import { DetalleRepuestosModule } from './detalle-repuestos/detalle-repuestos.module.js';
->>>>>>> bbf4a5e1267a76680c56519039ded4c1baad5936
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -21,12 +18,9 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       serviceId: 'AutoFix-NestJS',
     }),
     PrismaModule,
-<<<<<<< HEAD
     VehiculosModule,
-=======
     RepuestosModule,
     DetalleRepuestosModule,
->>>>>>> bbf4a5e1267a76680c56519039ded4c1baad5936
   ],
   controllers: [AppController],
   providers: [AppService],
